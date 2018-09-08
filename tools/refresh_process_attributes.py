886d2a11be9498fe11c48bda49b7247498246ad3
Runner-42 <wim.jacops@gmail.com>
2018-09-08 22:09:56 +0200

    Tool to send re-initialize event to process queue

---
+++ tools/refresh_process_attributes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import pika
+
+queue_name='IQ_RPI_INPUTBUTTON_PI3'
+
+if len(sys.argv) > 1:
+    queue_name = sys.argv[1]
+
+event = "P;REFRESH_PROCESS_ATTRIBUTES"
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue=queue_name)
+
+channel.basic_publish(exchange='HOMEDOMOTICA',
+                      routing_key=queue_name,
+                      body=event)
+
+print(" [x] Sent", event, " to", queue_name)
+connection.close()
