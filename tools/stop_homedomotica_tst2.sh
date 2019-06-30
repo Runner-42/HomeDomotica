2a9c4dce9a60e647215932c45a2c57c3f8f60ae7
Runner-42 <wim.jacops@gmail.com>
2019-06-30 13:01:50 +0200

    correct python program name in stop_homedomotica_tst script

--- tools/stop_homedomotica_tst2.sh
+++ tools/stop_homedomotica_tst2.sh
@@ -18,12 +18,12 @@ path_to_source_file=/home/homedomotica/environments/$active_environment/src
 pi_reference=TST2
 
 echo "Stopping 'inputbutton' process"
-python3 $path_to_source_files/send_stop.py IQ_RPI_INPUTBUTTON_$pi_reference
+python3 $path_to_source_files/send_stop_req.py IQ_RPI_INPUTBUTTON_$pi_reference
 echo "Stopping 'outputrelay' process"
-python3 $path_to_source_files/send_stop.py IQ_RPI_OUTPUTRELAY_$pi_reference
+python3 $path_to_source_files/send_stop_req.py IQ_RPI_OUTPUTRELAY_$pi_reference
 echo "Stopping 'outputlights' process"
-python3 $path_to_source_files/send_stop.py IQ_RPI_OUTPUTLIGHTS_$pi_reference
+python3 $path_to_source_files/send_stop_req.py IQ_RPI_OUTPUTLIGHTS_$pi_reference
 echo "Stopping 'outputdimmer' process"
-python3 $path_to_source_files/send_stop.py IQ_RPI_OUTPUTDIMMER_$pi_reference
+python3 $path_to_source_files/send_stop_req.py IQ_RPI_OUTPUTDIMMER_$pi_reference
 
 deactivate
