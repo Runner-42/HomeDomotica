58cd2bbe39894893f2fa286bfa628c271ac83c4c
Runner-42 <wim.jacops@gmail.com>
2019-07-04 20:44:53 +0200

    Start and stop scripts for production pi systems

---
+++ tools/start_homedomotica_pi2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+exec > >(tee -a "/var/log/homedomotica/HomeDomotica.log") 2>&1
+date
+
+if [[ $# -gt 0 ]]
+then
+    active_environment=$1
+else
+    active_environment=green
+fi
+
+echo "Starting Home Domotica processes on $active_environment environment"
+
+source /home/homedomotica/environments/$active_environment/bin/activate
+
+log_level=DEBUG
+path_to_source_file=/home/homedomotica/environments/$active_environment/src
+path_to_configuration_file=/home/homedomotica/environments/$active_environment/config
+pi_reference=pi2
+
+echo "Starting 'inputbutton' process"
+python3  $path_to_source_file/rpi_inputbutton_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+echo "Starting 'outputrelay' process"
+python3  $path_to_source_file/rpi_outputrelay_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+echo "Starting 'outputlights' process"
+python3  $path_to_source_file/rpi_outputlights_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+echo "Starting 'outputdimmer' process"
+python3  $path_to_source_file/rpi_outputdimmer_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+
+deactivate
