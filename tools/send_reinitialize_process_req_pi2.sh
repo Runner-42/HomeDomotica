103474637f1ec9579880822b4b1e3d0c4bfda2dd
Runner-42 <wim.jacops@gmail.com>
2019-07-06 11:23:50 +0200

    Initial scripts for loglevel and process initialisation

---
+++ tools/send_reinitialize_process_req_pi2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+exec > >(tee -a "/var/log/homedomotica/HomeDomotica.log") 2>&1
+date
+
+if [[ $# -gt 0 ]]
+then
+    active_environment=$1
+else
+    active_environment=green
+fi
+
+source /home/homedomotica/environments/$active_environment/bin/activate
+
+path_to_source_files=/home/homedomotica/environments/$active_environment/src
+pi_reference=PI2
+
+echo "Sending reinitialize process Request event on $active_environment environment"
+python3 $path_to_source_files/send_refresh_process_attributes_req.py IQ_RPI_INPUTBUTTON_$pi_reference
+python3 $path_to_source_files/send_refresh_process_attributes_req.py IQ_RPI_OUTPUTLIGHTS_$pi_reference
+python3 $path_to_source_files/send_refresh_process_attributes_req.py IQ_RPI_OUTPUTRELAY_$pi_reference
+python3 $path_to_source_files/send_refresh_process_attributes_req.py IQ_RPI_OUTPUTDIMMER_$pi_reference
+
+deactivate
