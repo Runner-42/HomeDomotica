3c5d341712b50c925d79c22d6f9d6ac787a5a7cf
Runner-42 <wim.jacops@gmail.com>
2019-07-08 19:38:32 +0200

    Fix incorrect keyword

--- src/rpi_homedomoticaconfigurationfile.py
+++ src/rpi_homedomoticaconfigurationfile.py
@@ -42,7 +42,7 @@ class RPiHomedomoticaConfigurationFile():
                       "[RPI_OUTPUTDIMMER_PI2]","[RPI_OUTPUTDIMMER_PI3]","[RPI_OUTPUTDIMMER_TST2]",\
                       "[RPI_OUTPUTRELAY]", "[RPI_OUTPUTRELAY_PI1]", "[RPI_OUTPUTRELAY_PI2]",\
                       "[RPI_OUTPUTRELAY_PI3]", "[RPI_OUTPUTRELAY_TST2]", "Port", "Host_IP",\
-                      "RPI_LIGHTSIMULATOR_TSTMGMT", "RPI_LIGHTSIMULATOR_MGMT",\
+                      "[RPI_LIGHTSIMULATOR_TSTMGMT]", "[RPI_LIGHTSIMULATOR_MGMT]",\
                       "Button00", "Button01", "Button02", "Button03", "Button04",\
                       "Button05", "Button06", "Button07", "Button10", "Button11", "Button12",\
                       "Button13", "Button14", "Button15", "Button16", "Button17", "Button20",\
