51de7172933f21c45ef243de92e8c86461cedda5
Runner-42 <wim.jacops@gmail.com>
2020-01-05 19:06:19 +0100

    Setup Tst1 environment

--- tools/start_homedomotica_tst1.sh
+++ tools/start_homedomotica_tst1.sh
@@ -22,11 +22,11 @@ pi_reference=tst1
 echo "Starting 'inputbutton' process"
 python3  $path_to_source_file/rpi_inputbutton_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
 #echo "Starting 'outputrelay' process"
-#python3  $path_to_source_file/rpi_outputrelay_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+python3  $path_to_source_file/rpi_outputrelay_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
 #echo "Starting 'outputlights' process"
-#python3  $path_to_source_file/rpi_outputlights_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+python3  $path_to_source_file/rpi_outputlights_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
 #echo "Starting 'outputdimmer' process"
-#python3  $path_to_source_file/rpi_outputdimmer_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
-echo "Starting 'outputventilator' process"
-python3  $path_to_source_file/rpi_outputventilator_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+python3  $path_to_source_file/rpi_outputdimmer_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
+#echo "Starting 'outputventilator' process"
+#python3  $path_to_source_file/rpi_outputventilator_$pi_reference.py -l $log_level -cfp $path_to_configuration_file &
 deactivate
