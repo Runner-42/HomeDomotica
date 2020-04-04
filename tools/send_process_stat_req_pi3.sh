87191650c4e0259253419d4443508d94ec57ad61
Wim Jacops <wim.jacops@gmail.com>
2020-04-04 15:19:35 +0200

    Bugfix send_process_stat_req script

--- tools/send_process_stat_req_pi3.sh
+++ tools/send_process_stat_req_pi3.sh
@@ -12,13 +12,13 @@ fi
 
 source /home/homedomotica/environments/$active_environment/bin/activate
 
-path_to_tool_files=/home/homedomotica/environments/$active_environment/tools
+path_to_src_files=/home/homedomotica/environments/$active_environment/src
 pi_reference=PI3
 
 echo "Sending Process Status Request event on $active_environment environment"
-python3 $path_to_tool_files/send_process_stat_req.py IQ_RPI_INPUTBUTTON_$pi_reference
-python3 $path_to_tool_files/send_process_stat_req.py IQ_RPI_OUTPUTLIGHTS_$pi_reference
-python3 $path_to_tool_files/send_process_stat_req.py IQ_RPI_OUTPUTRELAY_$pi_reference
-python3 $path_to_tool_files/send_process_stat_req.py IQ_RPI_OUTPUTDIMMER_$pi_reference
+python3 $path_to_src_files/send_process_stat_req.py IQ_RPI_INPUTBUTTON_$pi_reference
+python3 $path_to_src_files/send_process_stat_req.py IQ_RPI_OUTPUTLIGHTS_$pi_reference
+python3 $path_to_src_files/send_process_stat_req.py IQ_RPI_OUTPUTRELAY_$pi_reference
+python3 $path_to_src_files/send_process_stat_req.py IQ_RPI_OUTPUTDIMMER_$pi_reference
 
 deactivate
