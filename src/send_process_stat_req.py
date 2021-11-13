ae8669d98ee81426f69e0d888789370a2fc4ff9a
Wim Jacops <wim.jacops@gmail.com>
2021-11-13 19:41:55 +0100

    Enhancement issue#17 Prepare JSON based events

--- src/send_process_stat_req.py
+++ src/send_process_stat_req.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 import sys
 import pika
+import json
 
 if len(sys.argv) == 2:
     queue_name = sys.argv[1]
 else:
     print("Usage send_process_stat_req.py <Queue Name>")
 
-event = "P;PRINT_PROCESS_STATUS"
+message = {"Type": "Processing",
+           "Event": "PRINT_PROCESS_STATUS"}
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue=queue_name)
 
 channel.basic_publish(exchange='HOMEDOMOTICA',
                       routing_key=queue_name,
-                      body=event)
+                      body=json.dumps(message))
 
-print(" [x] Sent", event, " to ",queue_name)
+print(" [x] Sent", json.dumps(message), " to ", queue_name)
 connection.close()
